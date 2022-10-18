c2eff3d2d308a668ceee036252a5b45d8d4530dc
George Musker <george.musker@and.digital>
2022-10-18 11:35:56 +0100

    display each attendee

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [attendees, setAttendees] = useState([]);
 
   const getAttendees = async () => {
-      const response = await fetch('https://www.eventbriteapi.com/v3/events/443933827027/attendees/?token=GB33BZ3UZXJ5PO4VQ2PB');
-
-      console.log(await response.json());
-  }
+    const response = await fetch(
+      "https://www.eventbriteapi.com/v3/events/443933827027/attendees/?token=GB33BZ3UZXJ5PO4VQ2PB"
+    );
+    const { attendees } = await response.json();
+    setAttendees(attendees);
+  };
 
   return (
-    
     <div className="App">
-    <button onClick={getAttendees}>Get Attendees</button>
+      <div>
+        <label htmlFor="name">Attendee:</label>
+        <input type="text" name="name" />
+        <button onClick={getAttendees}>Get Attendees</button>
+      </div>
+      <div>
+        {attendees.map((attendee, index) => (
+          <div key={index}>
+            <pre>{JSON.stringify(attendee, null, 2)}</pre>
+            <pre>{JSON.stringify(attendee.profile, null, 2)}</pre>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
