9963e690ca1c74fc1893b2fa5c2cf706decc5cb5
Dylan Wright <dylan.wright@and.digital>
2022-10-18 10:44:02 +0100

    feat(inital-commit): request to get attendees

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const getAttendees = async () => {
+      const response = await fetch('https://www.eventbriteapi.com/v3/events/443933827027/attendees/?token=GB33BZ3UZXJ5PO4VQ2PB');
+
+      console.log(await response.json());
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={getAttendees}>Get Attendees</button>
     </div>
   );
 }
